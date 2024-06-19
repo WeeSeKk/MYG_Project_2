48f04689129638900d763f49e0ee7494c859ebbe
WeeSeK <113300702+WeeSeKk@users.noreply.github.com>
2024-06-19 19:21:08 +0200

    script

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,24 +1,83 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class UIManager : MonoBehaviour
 {
     VisualElement root;
-    TextField textField;
+    VisualElement errorBox;
+    TextField passwordTextField;
+    TextField emailTextField;
     Button button;
+    Label errorBoxLabel;
+    bool emailError;
+    bool passwordError;
     
-    // Start is called before the first frame update
     void Start()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
+        emailTextField = root.Q<TextField>("EmailTextField");
+        passwordTextField = root.Q<TextField>("PasswordTextField");
+        button = root.Q<Button>("LoginButton");
+        errorBox = root.Q<VisualElement>("ErrorBox");
+        errorBoxLabel = root.Q<Label>("ErrorBoxLabel");
+        ErrorBoxReset();
+
+        button.clickable.clicked += OnButtonClickedHandler;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnButtonClickedHandler()
     {
-        
+        if(emailTextField.text == "admin")
+        {
+            Debug.Log(emailTextField.text);
+        }
+        else
+        {
+            emailError = true;
+            Debug.Log("email error");
+        }
+        if(passwordTextField.text == "admin")
+        {
+            Debug.Log(passwordTextField.text);
+        }
+        else
+        {
+            passwordError = true;
+            Debug.Log("password error");
+        }
+
+        if(emailError == true && passwordError == false)
+        {
+            errorBoxLabel.text = "email error";
+            errorBox.visible = true;
+            errorBoxLabel.visible = true;
+            Invoke("ErrorBoxReset", 2f);
+        }
+        else if(emailError == false && passwordError == true)
+        {
+            errorBoxLabel.text = "password error";
+            errorBox.visible = true;
+            errorBoxLabel.visible = true;
+            Invoke("ErrorBoxReset", 2f);
+        }
+        else if(emailError == true && passwordError == true)
+        {
+            errorBoxLabel.text = "password and email error";
+            errorBox.visible = true;
+            errorBoxLabel.visible = true;
+            Invoke("ErrorBoxReset", 2f);
+        }
+    }
+
+    void ErrorBoxReset()
+    {
+        errorBox.visible = false;
+        errorBoxLabel.visible = false;
+        emailError = false;
+        passwordError = false;
     }
 }
